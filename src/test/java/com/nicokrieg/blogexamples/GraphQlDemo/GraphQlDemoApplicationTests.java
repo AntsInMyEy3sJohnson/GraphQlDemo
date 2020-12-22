8fbfe17e31b95e4e902902c217803789d58c3c61
Nico Krieg <n.krieg123@googlemail.com>
2020-12-22 19:19:06 +0100

    Add random port web environment to 'ContextLoads' test.

--- src/test/java/com/nicokrieg/blogexamples/GraphQlDemo/GraphQlDemoApplicationTests.java
+++ src/test/java/com/nicokrieg/blogexamples/GraphQlDemo/GraphQlDemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.nicokrieg.blogexamples.GraphQlDemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GraphQlDemoApplicationTests {
 
 	@Test
